59806ca7c8316daa64c59be12b517ff25ae85338
DiegoHdz7 <diego.hdz.glez@gmail.com>
2024-02-26 18:49:32 -0600

    hbskjbf

---
+++ sesp-backend/src/test/java/sesp/server/sespbackend/SespBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sesp.server.sespbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SespBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
